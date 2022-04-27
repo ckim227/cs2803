cf51daf9f5e1ca25cbfa87ad678f492fa95a93e7
Celestin <chering6@gatech.edu>
2022-04-27 13:14:56 -0400

    Regex

--- public/js/uploadRecipe.js
+++ public/js/uploadRecipe.js
@@ -5,31 +5,35 @@ let recipeName = document.getElementById("recipeName");
 
 function saveLink(event){
     event.preventDefault()
-    if (recipeLink.value.length > 0 && recipeName.value.length > 0) {
-        let xhr = new XMLHttpRequest()
-        xhr.addEventListener("load", responseHandler)
-        query=`link=${recipeLink.value}&recipeName=${recipeName.value}`
-        // when submitting a GET request, the query string is appended to URL
-        // but in a POST request, do not attach the query string to the url
-        // instead pass it as a parameter in xhr.send()
-        url = "/saveLinkedRecipe";
-        xhr.responseType = "json";   
-        xhr.open("POST", url)
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-        // notice the query string is passed as a parameter in xhr.send()
-        // this is to prevent the data from being easily sniffed
-        xhr.send(query)
-    } 
-    let message = document.getElementById("message");
-    if (recipeLink.value.length == 0 && recipeName.value.length == 0) {
-        message.classList.remove("invisible");
-        message.innerText = "Please enter a recipe name and link!"
-    } else if (recipeLink.value.length == 0) {
+    var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+    if (recipeLink.matches(regex)) {
+        if (recipeName.value.length > 0) {
+            let xhr = new XMLHttpRequest()
+            xhr.addEventListener("load", responseHandler)
+            query=`link=${recipeLink.value}&recipeName=${recipeName.value}`
+            // when submitting a GET request, the query string is appended to URL
+            // but in a POST request, do not attach the query string to the url
+            // instead pass it as a parameter in xhr.send()
+            url = "/saveLinkedRecipe";
+            xhr.responseType = "json";   
+            xhr.open("POST", url)
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+            // notice the query string is passed as a parameter in xhr.send()
+            // this is to prevent the data from being easily sniffed
+            xhr.send(query)
+        } 
+        let message = document.getElementById("message");
+        if (recipeName.value.length == 0) {
+            message.classList.remove("invisible");
+            message.innerText = "Please enter a name!"
+        }
+    } if (recipeLink.value.length == 0) {
         message.classList.remove("invisible");
         message.innerText = "Please enter a recipe link!"
-    } else if (recipeName.value.length == 0) {
+    } else {
         message.classList.remove("invisible");
-        message.innerText = "Please enter a name!"
+        message.innerText = "Please enter a valid link!"
     }
 }
 
