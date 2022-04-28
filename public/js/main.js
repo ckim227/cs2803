06f970ca8edb67febc488262eb4f11591c4480de
Celestin <chering6@gatech.edu>
2022-04-28 09:34:04 -0400

    Fixed save comment button

--- public/js/main.js
+++ public/js/main.js
@@ -51,7 +51,7 @@ function responseHandlerSaved() {
                 <form method="POST" action ="/saveComment">
                     <label for="${"comment"+ index}">Comments:</label>
                     <input type="text" class="form-control mb-3 overflow-auto" id=${"comment"+ index} rows="3" value="${recipe.comment}"></input> 
-                    <button type="submit" class="btn btn-outline-dark btn-sm" id=${"button"+index}>Save Comment</button>
+                    <button onmousedown="event.preventDefault()" type="submit" class="btn btn-outline-dark btn-sm" id=${"button"+index}>Save Comment</button>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-check d-none" viewBox="0 0 16 16" id="${"check" + index}">
                         <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"></path>
                     </svg>
